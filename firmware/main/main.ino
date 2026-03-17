d8c3bc5b98c7b7e5ccc3cb26f69bdfaa2c5379cc
Viyapakan Shanmugam <147246267+Viyapakan@users.noreply.github.com>
2026-03-18 00:57:17 +0530

    feat: successful board config and basic led blink test

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+// Built-in LED is usually on GPIO 2 for these ESP32 boards
+#define LED_PIN 2
 
+void setup() {
+  // Initialize serial communication at 115200 baud rate
+  Serial.begin(115200);
+  
+  // Initialize the LED pin as an output
+  pinMode(LED_PIN, OUTPUT);
+  
+  Serial.println("ESP32 is alive! Starting Blink Test...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_PIN, HIGH);  // Turn LED ON
+  Serial.println("LED ON");
+  delay(1000);                  // Wait 1 second
+  
+  digitalWrite(LED_PIN, LOW);   // Turn LED OFF
+  Serial.println("LED OFF");
+  delay(1000);                  // Wait 1 second
 }
